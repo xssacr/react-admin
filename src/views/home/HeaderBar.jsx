0a90bfc5835a019ee511b86083acbae4bfb35f0a
小赛赛爱吃肉 <xssacr@aliyun.com>
2020-05-07 21:30:18 +0800

    退出功能

---
+++ src/views/home/HeaderBar.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Layout, Dropdown, Row, Col, Menu, Modal,message } from "antd";
+import { DownOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
+import { withRouter } from "react-router-dom";
+
+class HeaderBar extends Component {
+  // 退出的逻辑
+  logoutHandler = ({ key }) => {
+    if (key === "logout") {
+      // 温馨提示
+      Modal.confirm({
+        title: "确定要退出吗?",
+        icon: <ExclamationCircleOutlined />,
+        cancelText: "取消",
+        okText:'确定',
+        onOk: () => {
+          // 清除 cookie 的数据
+          sessionStorage.removeItem('userInfo');
+          // 退出成功的提示
+          message.success('退出成功!')
+          // 跳转到 登录
+          this.props.history.push("/login");
+        },
+        onCancel() {},
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Layout.Header className="site-layout-background" style={{ padding: 0 }}>
+        <Row>
+          <Col span={6} offset={18}>
+            <Dropdown
+              overlay={
+                <Menu onClick={this.logoutHandler}>
+                  <Menu.Item key="logout">退出登录</Menu.Item>
+                </Menu>
+              }
+              className="userinfo-menu"
+            >
+              <span
+                className="ant-dropdown-link header-userinfo"
+                onClick={(e) => e.preventDefault()}
+              >
+                hello {this.props.username} <DownOutlined />
+              </span>
+            </Dropdown>
+          </Col>
+        </Row>
+      </Layout.Header>
+    );
+  }
+}
+
+export default withRouter(HeaderBar);
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,14 +1,63 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { DesktopOutlined, UserOutlined } from "@ant-design/icons";
+
+import "./home.style.scss";
+import HeaderBar from './HeaderBar'
+
+const {  Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
 class Home extends Component {
   constructor() {
     super();
     this.state = {
       username: "",
+      collapsed: false,
     };
   }
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   render() {
-    return <div>hello, {this.state.username}</div>;
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              用户管理
+            </Menu.Item>
+            <SubMenu key="sub2" icon={<DesktopOutlined />} title="数据管理">
+              <Menu.Item key="6">菜谱分类管理</Menu.Item>
+              <Menu.Item key="8">推荐数据管理</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+
+          <HeaderBar username={this.state.username}></HeaderBar>
+         
+          <Content style={{ margin: "0 16px" }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    );
   }
 
   componentDidMount() {
@@ -16,6 +65,7 @@ class Home extends Component {
     this.auth();
   }
 
+  // 验证权限的方法
   auth() {
     // 判断用户是否有权限
     let userInfo = sessionStorage.getItem("userInfo");
